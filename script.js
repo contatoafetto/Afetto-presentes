2528b75021c3cbae8f734a3ca32e8c9a2f84e1fc
contatoafetto <matheus.maron.workspace@gmail.com>
2026-05-04 09:48:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Scroll suave para exemplos
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
